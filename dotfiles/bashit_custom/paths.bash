85e607105a161c1ffb9d2e0b845f8c1f2b531114
Nick Wei <n.wei@pivotal.io>
2016-04-25 16:44:29 -0700

    Add user's home to path
    Signed-off-by: Anand Gaitonde <agaitonde@pivotal.io>

--- dotfiles/bashit_custom/paths.bash
+++ dotfiles/bashit_custom/paths.bash
@@ -5,3 +5,6 @@ export PATH=$GOPATH/bin:$PATH
 # most recently build cf should be in path
 export WORKSPACE=$GOPATH/src/github.com/cloudfoundry/cli
 export PATH=$WORKSPACE/out:$PATH
+
+# add bin from homedir to path
+export PATH=$HOME/bin:$PATH
