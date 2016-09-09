999ae7ac55dfa1e9a40bf798a2d63adb51ad2ab2
Luan Santos <lsantos@pivotal.io>
2016-09-09 08:40:12 -0700

    update cli gopath to code.cloudfoundry.org
    Signed-off-by: Nick Wei <nwei@pivotal.io>

--- dotfiles/bashit_custom/paths.bash
+++ dotfiles/bashit_custom/paths.bash
@@ -3,7 +3,7 @@ export GOPATH=~/go
 export PATH=$GOPATH/bin:$PATH
 
 # most recently build cf should be in path
-export WORKSPACE=$GOPATH/src/github.com/cloudfoundry/cli
+export WORKSPACE=$GOPATH/src/code.cloudfoundry.org/cli
 export PATH=$WORKSPACE/out:$PATH
 
 # add bin from homedir to path
