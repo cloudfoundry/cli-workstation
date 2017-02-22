4afba367c19db16d115e5893e672196da1dff98f
Anand Gaitonde <agaitonde@pivotal.io>
2017-02-21 19:11:02 -0800

    add all the things for a ubuntu machine to run properly

--- dotfiles/bashit_custom_linux/paths.bash
+++ dotfiles/bashit_custom_linux/paths.bash
@@ -2,9 +2,9 @@
 export PATH=$HOME/bin:$PATH
 
 # Go
-export GOROOT=/opt/golang/go1.7.5
+export GOROOT=/usr/local/golang/go1.7.5
 export GOPATH=~/go
-export PATH=$GOPATH/bin:$PATH
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 
 # most recently build cf should be in path
 export PATH=$GOPATH/src/code.cloudfoundry.org/cli/out:$PATH
--- install-debian.sh
+++ install-debian.sh
@@ -6,18 +6,22 @@ GO_VERSION="1.7.5"
 
 # Add any required repositories
 sudo add-apt-repository -y ppa:neovim-ppa/unstable
+sudo add-apt-repository -y ppa:webupd8team/atom
+
 
 # Update/Upgrade to the latest
-sudo apt-get update
-sudo apt-get upgrade -y
+sudo apt update
+sudo apt upgrade -y
 
 # Install system dependancies
-sudo apt-get install -y bash-completion chromium-browser curl htop openssh-server software-properties-common tree 
+sudo apt install -y bash-completion chromium-browser curl htop openssh-server software-properties-common tree fastd
 
 # Install development dependancies
-sudo apt-get install -y awscli bzr direnv exuberant-ctags git jq lastpass-cli neovim nodejs ruby silversearcher-ag tig tmux virtualbox-qt
+sudo apt install -y atom awscli bzr direnv exuberant-ctags git jq lastpass-cli neovim nodejs npm python3-pip ruby silversearcher-ag tig tmux virtualbox-qt
 
-sudo apt-get clean
+# Cleanup cache
+sudo apt -y autoremove
+sudo apt autoclean
 
 # Install fly
 if [[ ! -x $HOME/bin/fly ]]; then
@@ -38,9 +42,7 @@ clone_into_workspace() {
 
 WORKSPACE_GIT_REPOS=(
   https://github.com/cloudfoundry-incubator/cf-routing-release
-  https://github.com/cloudfoundry-incubator/cli-plugin-repo
   https://github.com/cloudfoundry-incubator/cli-workstation
-  https://github.com/cloudfoundry-incubator/diego-enabler
   https://github.com/cloudfoundry-incubator/diego-release
   https://github.com/cloudfoundry/bosh-lite
   https://github.com/cloudfoundry/cf-release
@@ -50,23 +52,6 @@ WORKSPACE_GIT_REPOS=(
 
 for repo in "${WORKSPACE_GIT_REPOS[@]}"; do
   clone_into_workspace $repo
-done
-
-# Clone Go repos into the correct gopath
-clone_into_go_path() {
-  DIR="${HOME}/go/src/${1}"
-  if [[ ! -d $DIR ]]; then
-    git clone "https://${1}" $DIR
-  fi
-}
-
-GO_REPOS=(
-  code.cloudfoundry.org/cli
-  github.com/cloudfoundry/cf-acceptance-tests
-)
-
-for repo in "${GO_REPOS[@]}"; do
-  clone_into_go_path $repo
 done
 
 # Install/Upgrade BashIT
@@ -92,12 +77,91 @@ ln -sf $HOME/workspace/cli-workstation/dotfiles/vimfiles/vimrc.local $HOME/.vimr
 ln -sf $HOME/workspace/cli-workstation/dotfiles/git/gitconfig $HOME/.gitconfig
 ln -sf $HOME/workspace/cli-workstation/dotfiles/git/git-authors $HOME/.git-authors
 
+ln -sf $HOME/workspace/cli-workstation/dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+if [[ ! -d ~/.tmux/plugins/tpm ]]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+# Install diff-so-fancy for better diffing
+sudo npm install -g diff-so-fancy
+
 # Install go if it's not installed
 if [[ -z $(which go) ]]; then
-  sudo mkdir -p /opt/golang
+  sudo mkdir -p /usr/local/golang
+  sudo chown pivotal:pivotal /usr/local/golang
+  mkdir -p $HOME/go/src
   curl -L "https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz" > /tmp/go.tgz
-  tar -C /tmp -xzf go.tgz
-  mv go /opt/golang/go$GO_VERSION
-  export GOROOT=/opt/golang/go$GO_VERSION
+  tar -C /usr/local/golang -xzf /tmp/go.tgz
+  mv /usr/local/golang/go /usr/local/golang/go$GO_VERSION
+  export GOROOT=/usr/local/golang/go$GO_VERSION
+  export GOPATH=$HOME/go
   rm /tmp/go.tgz
+fi
+
+# Clone Go repos into the correct gopath
+clone_into_go_path() {
+  DIR="${HOME}/go/src/${1}"
+  if [[ ! -d $DIR ]]; then
+    mkdir -p $(dirname $DIR)
+    git clone "https://${1}" $DIR
+    ln -s $DIR $HOME/workspace/$(basename $DIR)
+  fi
+}
+
+GO_REPOS=(
+  github.com/cloudfoundry/cf-acceptance-tests
+  github.com/cloudfoundry-incubator/cli-plugin-repo
+  github.com/cloudfoundry-incubator/diego-enabler
+)
+
+for repo in "${GO_REPOS[@]}"; do
+  clone_into_go_path $repo
+done
+
+# Clone CLI Repo
+if [[ ! -d "${GOPATH}/src/code.cloudfoundry.org/cli" ]]; then
+  mkdir -p "${GOPATH}/src/code.cloudfoundry.org"
+  cd "${GOPATH}/src/code.cloudfoundry.org"
+  git clone "https://github.com/cloudfoundry/cli"
+fi
+
+# Install common utilities
+GO_UTILS=(
+  github.com/onsi/ginkgo/ginkgo
+  github.com/onsi/gomega
+  github.com/maxbrunsfeld/counterfeiter
+  github.com/FiloSottile/gvt
+  github.com/tools/godep
+  github.com/jteeuwen/go-bindata/...
+  github.com/nicksnyder/go-i18n/goi18n
+  github.com/krishicks/i18n4go/i18n4go
+  github.com/alecthomas/gometalinter
+  github.com/git-duet/git-duet/...
+)
+
+for gopkg in "${GO_UTILS[@]}"; do
+  echo Getting/Updating $gopkg
+  GOPATH=$HOME/go go get -u $gopkg
+done
+
+# install bosh
+if [[ -z $(which bosh) ]]; then
+  sudo gem install --no-document bosh_cli
+fi
+
+# install bundler
+if [[ -z $(which bundler) ]]; then
+  sudo gem install --no-document bundler
+fi
+
+sudo gem update --no-document bosh_cli bundler
+
+# Install Luan's Vim config
+if [[ -d $HOME/.vim ]]; then
+  pip3 install --upgrade neovim
+  $HOME/.vim/update
+else
+  pip3 install neovim
+  git clone https://github.com/luan/vimfiles.git $HOME/.vim
+  $HOME/.vim/install
 fi
