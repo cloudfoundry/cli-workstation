f9fc0085809cc799c992d31bacaf842fc6c95428
Anand Gaitonde <agaitonde@pivotal.io>
2017-02-28 11:52:40 -0800

    add bosh2 alias if bosh-cli exists
    Signed-off-by: Stu Pollock <spollock@pivotal.io>

--- dotfiles/bashit_custom/aliases.bash
+++ dotfiles/bashit_custom/aliases.bash
@@ -2,3 +2,7 @@ alias vim=nvim
 
 alias grp="ginkgo -r -nodes=4"
 alias fl="fly -t ci login -c https://p-concourse.wings.cf-app.com -n system-team-cli-cf-cli-619e"
+
+if [[ ! -z $(which bosh-cli) ]]; then
+  alias bosh2=bosh-cli
+fi
