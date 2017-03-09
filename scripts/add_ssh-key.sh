24a5de179f36dca1ea6bfec537cb2e5a33d5d592
Matthew Boedicker <mboedicker@pivotal.io>
2017-03-09 09:11:04 -0800

    script to add get ssh key from lastpass and add to ssh agent

---
+++ scripts/add_ssh-key.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+KEY_EXPIRES_AT=$(date --date=18:00 +%s)
+NOW=$(date +%s)
+KEY_LIFETIME=$(($KEY_EXPIRES_AT - $NOW))
+if [ "$KEY_LIFETIME" -lt "1" ]; then
+  KEY_LIFETIME=1h
+fi
+
+lpass show github --notes | ssh-add -t $KEY_LIFETIME -
