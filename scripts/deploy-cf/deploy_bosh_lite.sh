c87ef233fa00bfb69b8adfd9aad8e4b888a64178
Matthew Boedicker <mboedicker@pivotal.io>
2017-03-09 15:02:30 -0800

    use 3363.9 stemcell
    Signed-off-by: Renee Chu <rchu@pivotal.io>

--- scripts/deploy-cf/deploy_bosh_lite.sh
+++ scripts/deploy-cf/deploy_bosh_lite.sh
@@ -45,7 +45,9 @@ bosh2 \
 
 export BOSH_CLIENT_SECRET=`bosh2 int ~/deployments/vbox/creds.yml --path /admin_password`
 
-bosh2 upload-stemcell https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent
+# if cf-deployment is not using the latest
+bosh2 upload-stemcell https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent?v=3363.9
+# bosh2 upload-stemcell https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent
 
 cd ~/workspace/cf-deployment/
 
