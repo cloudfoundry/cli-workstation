d3d7c7419dc85b2c49ef04534b916e44d1c6c407
Anand Gaitonde <agaitonde@pivotal.io>
2017-04-13 10:48:42 -0700

    use Vim instead of NeoVim

--- dotfiles/bashit_custom/env.bash
+++ dotfiles/bashit_custom/env.bash
@@ -2,8 +2,8 @@
 export GIT_DUET_GLOBAL=true
 export GIT_DUET_ROTATE_AUTHOR=true
 
-# set nvim to be the default editor
-export EDITOR=nvim
+# set vim to be the default editor
+export EDITOR=vim
 
 # set app dir to be /Applications
 HOMEBREW_CASK_OPTS='--appdir=/Applications'
