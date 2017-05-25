6570754668577bd1b8e20bcd846d49e7bd935710
Anand Gaitonde <agaitonde@pivotal.io>
2017-05-25 12:22:12 -0700

    updating alias for new concourse instance
    Signed-off-by: Tushar Singal <tsingal@pivotal.io>

--- dotfiles/bashit_custom/aliases.bash
+++ dotfiles/bashit_custom/aliases.bash
@@ -1,7 +1,7 @@
 # alias vim=nvim
 
 alias grp="ginkgo -r -nodes=4"
-alias fl="fly -t ci login -c https://p-concourse.wings.cf-app.com -n system-team-cli-cf-cli-619e"
+alias fl="fly -t ci login -c https://wings.concourse.ci -n cf-cli"
 alias cfbl="cf login -a api.bosh-lite.com --skip-ssl-validation -u admin -p admin"
 
 if [[ ! -z $(which bosh-cli) ]]; then
