b6a67a908ced4c21cbd8555d76d7c2cf661f1a3b
Anand Gaitonde <agaitonde@pivotal.io>
2018-07-19 14:44:51 -0700

    update to use git co-author

--- dotfiles/bashit_custom/env.bash
+++ dotfiles/bashit_custom/env.bash
@@ -1,6 +1,6 @@
 # git-duet
 export GIT_DUET_GLOBAL=true
-export GIT_DUET_ROTATE_AUTHOR=true
+export GIT_DUET_CO_AUTHORED_BY=1
 
 # set vim to be the default editor
 export EDITOR=vim
--- scripts/fix-committer.sh
+++ scripts/fix-committer.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-committer=$(git show HEAD -s --pretty="tformat:%b" | grep Signed-off | cut -d: -f2)
+committer=$(git show HEAD -s --pretty="tformat:%b" | grep Co-authored-by | cut -d: -f2)
 if git branch -a --contains=HEAD | grep remotes; then
     # return immediately if the current commit has been pushed remotely
     exit 0
 fi
 if [ "x$committer" != "x" ]; then
     # extract the name and email of the committer from the signed-of line
-    # e.g. "Signed-off-by: Ed King <eking@pivotal.io>"
+    # e.g. "Co-authored-by: Ed King <eking@pivotal.io>"
     GIT_COMMITTER_EMAIL=$(echo $committer | cut -d\< -f2 | cut -d\> -f1)
     GIT_COMMITTER_NAME=$(echo $committer | cut -d\< -f1)
     # remove leading and trailing white spaces
