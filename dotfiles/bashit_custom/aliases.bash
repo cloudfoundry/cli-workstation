bf6851cd27c0accb3a52eb02b6ca58672f46fedc
Will Murphy <wmurphy@pivotal.io>
2018-08-17 14:08:15 -0700

    better s alias than weird fasd thing
    Co-authored-by: Tom Viehman <tviehman@pivotal.io>

--- dotfiles/bashit_custom/aliases.bash
+++ dotfiles/bashit_custom/aliases.bash
@@ -3,6 +3,7 @@
 alias grp="ginkgo -r -nodes=4"
 alias fl="fly -t ci login -c https://wings.pivotal.io -n cf-cli -b"
 alias cfbl="cf login -a api.bosh-lite.com --skip-ssl-validation -u admin -p admin"
+alias s="git status"
 
 if [[ ! -z $(which bosh-cli) ]]; then
   alias bosh=bosh-cli
