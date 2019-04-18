25ca81c8456e6aab7793a75e1f3021fcd3fc2f11
Alexander Berezovsky <aberezovsky@pivotal.io>
2019-04-18 15:07:11 -0700

    Refactor where_my_bosh_lites_at
    Move private build_bosh_lite_output inside the where_my_bosh_lites_at
    contect. Make output a local variable.

--- dotfiles/bashit_custom/where-my-bosh-lites-at.bash
+++ dotfiles/bashit_custom/where-my-bosh-lites-at.bash
@@ -1,40 +1,44 @@
 #!/bin/bash
 
 
-PROJECT_ID=2105761
+function where_my_bosh_lites_at() {
 
-function build_bosh_lite_output {
-  file="$( basename "${file}" )"
-  author="$(git log --max-count=1 --pretty='format:%an' "${file}")"
-  committer="$(git log --max-count=1 --pretty='format:%cn' "${file}")"
-  claimed_since="$(git log --max-count=1 --pretty='format:%ar' "${file}")"
-  if [ "${author}" != "${committer}" ]; then
-    claimed_by="${author}+${committer}"
-  else
-    claimed_by="${author}"
-  fi
+  local output
 
-  if $(git log --max-count=1 --pretty='format:%s' ${file} | grep -q CI-claim); then
-    ci_name=Spikeline
-    if $(git log --max-count=1 --pretty='format:%s' ${file} | grep -q CLI-CI-claim); then
-      ci_name=Spikecline
+  PROJECT_ID=2105761
+
+  function build_bosh_lite_output {
+    file="$( basename "${file}" )"
+    author="$(git log --max-count=1 --pretty='format:%an' "${file}")"
+    committer="$(git log --max-count=1 --pretty='format:%cn' "${file}")"
+    claimed_since="$(git log --max-count=1 --pretty='format:%ar' "${file}")"
+    if [ "${author}" != "${committer}" ]; then
+      claimed_by="${author}+${committer}"
+    else
+      claimed_by="${author}"
     fi
 
-    claimed_by="$ci_name on behalf of $claimed_by"
-    workstation=CI
-    # Assume the last field is the story ID and extract it.
-    story_id="$(git log --max-count=1 --pretty='format:%s' "${file}" | tr -d '#' | awk '{print $NF}')"
-    story="pivotaltracker.com/story/show/${story_id}"
-    story_state=$(curl -s "https://www.pivotaltracker.com/services/v5/projects/${PROJECT_ID}/stories/${story_id}" | jq -r ."current_state")
-    output="${output}${file}\t${claimed_by}\t${workstation}\t${claimed_since}\t${story} (${story_state})\n"
-  else
-    workstation="$(git log --max-count=1 --pretty='format:%s' "${file}" | sed -E "s/^manually claim [^[:space:]]+ on ([^[:space:]]+).*$/\1/")"
-    story='unknown'
-    output="${output}${file}\t${claimed_by}\t${workstation}\t${claimed_since}\t${story}\n"
-  fi
-}
+    if $(git log --max-count=1 --pretty='format:%s' ${file} | grep -q CI-claim); then
+      ci_name=Spikeline
+      if $(git log --max-count=1 --pretty='format:%s' ${file} | grep -q CLI-CI-claim); then
+        ci_name=Spikecline
+      fi
 
-function where_my_bosh_lites_at() {
+      claimed_by="$ci_name on behalf of $claimed_by"
+      workstation=CI
+      # Assume the last field is the story ID and extract it.
+      story_id="$(git log --max-count=1 --pretty='format:%s' "${file}" | tr -d '#' | awk '{print $NF}')"
+      story="pivotaltracker.com/story/show/${story_id}"
+      story_state=$(curl -s "https://www.pivotaltracker.com/services/v5/projects/${PROJECT_ID}/stories/${story_id}" | jq -r ."current_state")
+      output="${output}${file}\t${claimed_by}\t${workstation}\t${claimed_since}\t${story} (${story_state})\n"
+    else
+      workstation="$(git log --max-count=1 --pretty='format:%s' "${file}" | sed -E "s/^manually claim [^[:space:]]+ on ([^[:space:]]+).*$/\1/")"
+      story='unknown'
+      output="${output}${file}\t${claimed_by}\t${workstation}\t${claimed_since}\t${story}\n"
+    fi
+  }
+
+
   # Colors
   local red blue
   red='\033[0;31m'
@@ -75,6 +79,8 @@ function where_my_bosh_lites_at() {
   popd > /dev/null
   echo -e "$output" | column -t -s $'\t'
 }
+
+export -f where_my_bosh_lites_at
 
 function select_bosh_lite() {
   local cf_target=$(where_my_bosh_lites_at | grep ago | fzf | cut -d ' ' -f1)
