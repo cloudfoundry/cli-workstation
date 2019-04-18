09b9a1788bdef6c6a8e447b7bb711a3bbbfff129
Alexander Berezovsky <aberezovsky@pivotal.io>
2019-04-17 23:14:33 -0700

    Update cli theme api color

--- dotfiles/bashit_custom_themes/cli.theme.bash
+++ dotfiles/bashit_custom_themes/cli.theme.bash
@@ -41,7 +41,7 @@ cli_flags() {
 
 function prompt_command() {
     #PS1="${bold_cyan}$(scm_char)${green}$(scm_prompt_info)${purple}$(ruby_version_prompt) ${yellow}\h ${reset_color}in ${green}\w ${reset_color}\n${green}→${reset_color} "
-    PS1="\n$(battery_char) $(__bobby_clock)${yellow}$(ruby_version_prompt)$(cli_flags)$(get_cf_api)${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_prompt_char_info) ${green}→${reset_color} "
+    PS1="\n$(battery_char) $(__bobby_clock)${yellow}$(ruby_version_prompt)$(cli_flags)${reset_color}$(get_cf_api)${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_prompt_char_info) ${green}→${reset_color} "
 }
 
 THEME_SHOW_CLOCK_CHAR=${THEME_SHOW_CLOCK_CHAR:-"true"}
