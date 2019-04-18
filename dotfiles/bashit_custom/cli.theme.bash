89cf8fa3edf3d648c092e7553bf909494aa165b3
Alexander Berezovsky <aberezovsky@pivotal.io>
2019-04-17 22:51:31 -0700

    Refactor theme prompt

--- dotfiles/bashit_custom/cli.theme.bash
+++ dotfiles/bashit_custom/cli.theme.bash
@@ -22,22 +22,26 @@ __bobby_clock() {
 }
 
 get_cf_api() {
-  env | grep CF_INT_API | cut -d= -f2 | sed 's/.lite.cli.fun//'
+  api_name=$(env | grep CF_INT_API | cut -d= -f2 | sed 's/.lite.cli.fun//')
+
+  if [[ -n "$api_name" ]]; then
+    echo "$api_name "
+  fi
 }
 
-ruby_version_prompt() {
+cli_flags() {
   if [ "${TARGET_V7,,}" = "true" ]; then
     output="V7 $output"
   fi
   if [ "${CF_EXPERIMENTAL_LOGIN,,}" = "true" ]; then
     output="EXP $output"
   fi
-  echo $output
+  echo "$output"
 }
 
 function prompt_command() {
     #PS1="${bold_cyan}$(scm_char)${green}$(scm_prompt_info)${purple}$(ruby_version_prompt) ${yellow}\h ${reset_color}in ${green}\w ${reset_color}\n${green}→${reset_color} "
-    PS1="\n$(battery_char) $(__bobby_clock)${yellow}$(ruby_version_prompt) $(get_cf_api) ${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_prompt_char_info) ${green}→${reset_color} "
+    PS1="\n$(battery_char) $(__bobby_clock)${yellow}$(ruby_version_prompt)$(cli_flags)$(get_cf_api)${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_prompt_char_info) ${green}→${reset_color} "
 }
 
 THEME_SHOW_CLOCK_CHAR=${THEME_SHOW_CLOCK_CHAR:-"true"}
