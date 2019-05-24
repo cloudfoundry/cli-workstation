3ca0f89872e0fa5fc024243a0c81e0130c2ae9d6
Alexander Berezovsky <aberezovsky@pivotal.io>
2019-05-24 10:42:17 -0700

    Link UI scale script to ~/bin/

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -153,6 +153,8 @@ ln -sf $HOME/workspace/cli-workstation/dotfiles/bashit_custom_linux/* $HOME/.bas
 ln -sf $HOME/workspace/cli-workstation/dotfiles/git/gitconfig $HOME/.gitconfig_include
 ln -sf $HOME/workspace/cli-workstation/dotfiles/git/git-authors $HOME/.git-authors
 
+ln -sf $HOME/workspace/cli-workstation/scripts/ui-scale $HOME/bin/
+
 # Setup gitconfig
 if [[ -L $HOME/.gitconfig ]]; then
   rm $HOME/.gitconfig
